90dbb8fde1a53b9112a13fa6a74a86cf38e76603
Tsingyun <imtsingyun@gmail.com>
2020-09-26 22:51:50 +0800

    当一个线程调用共享对象的wait（）方法被阻塞挂起后， 如果其他线程中断了该线程， 则该线程会抛出InterruptedException 异常并返回。

---
+++ basic01-thread/src/main/java/org/mindidea/demo01/Demo04.java
@@ -0,0 +1,15 @@
+/*
+ * @class Demo04
+ * @package org.mindidea.demo01
+ * @date 2020/9/26 22:48
+ * Copyright (c) 2020 MindIdea.org, All Rights Reserved.
+ */
+package org.mindidea.demo01;
+
+/**
+ * @author Tsingyun(青雲)
+ * @version V1.0
+ * @createTime 2020/9/26 22:48
+ */
+public class Demo04 {
+}
